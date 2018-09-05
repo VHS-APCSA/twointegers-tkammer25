fed23be874ed417a1bc9565037b014f90ba0f591
bob <bob@APCSA>
2018-09-05 14:54:19 -0700

    Worked on beginning of assignment: private inst varb and twoconstructors

---
+++ src/TwoIntegers.java
@@ -0,0 +1,17 @@
+public class TwoIntegers 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+
+}
