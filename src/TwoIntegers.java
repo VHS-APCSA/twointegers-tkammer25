911050aeef0f6447574141674faff980de668505
bob <bob@APCSA>
2018-09-06 15:38:45 -0700

    Worked on 2.16, not done

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -13,5 +13,43 @@ public class TwoIntegers
 		this.num1 = num1;
 		this.num2 = num2;
 	}
-
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	public int getNum2()
+	{
+		return num2;
+	}
+	public void setNum2(int num2)
+	{
+		this.num2= num2;
+	}
+	public String arithmetic()
+	{
+		int sum = this.num1 + this.num2;
+		int product = this.num1 * this.num2;
+		int difference = this.num1 - this.num2;
+		int quotient = this.num1 / this.num2;
+		return num1 + " + " + num2 + " = " + sum
+		+ " // " + num1 + " * " + num2 + " = " + product
+		+ " // " + num1 + " - " + num2 + " = " + difference
+		+ " // " + num1 + " / " + num2 + " = " + quotient;
+	}
+	public String larger()
+	{
+		if(num1 > num2)
+		{
+			return num1;
+		}
+		else if(num2 > num1)
+		{
+			return num1;
+		}
+		else
+	}
 }
